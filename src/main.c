8318cacb3da9879c242e2d6ee3fcd3f4b066d3c7
enzocipriani <egcipriani@hotmail.com>
2022-12-13 19:15:51 -0300

    restante

--- src/main.c
+++ src/main.c
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <unistd.h>
 
 #define MAX_SEGMENTOS 1000
 #define TAM_SEGMENTO 10
+
 
 typedef struct {
     int x;
@@ -28,25 +30,55 @@ typedef struct {
 
 Cobra *cria_cobra(){
     Cobra *cobra = (Cobra*)malloc(sizeof(Cobra));
-    cobra->segmentos[0].x = 40;
-    cobra->segmentos[0].y = 0;   
-    cobra->segmentos[1].x = 30;
-    cobra->segmentos[1].y = 0;    
-    cobra->segmentos[2].x = 20;
-    cobra->segmentos[2].y = 0;    
-    cobra->segmentos[3].x = 10;
-    cobra->segmentos[3].y = 0;    
+    cobra->segmentos[0].x = 320;
+    cobra->segmentos[0].y = 240;   
+    cobra->segmentos[1].x = 310;
+    cobra->segmentos[1].y = 240;    
+    cobra->segmentos[2].x = 300;
+    cobra->segmentos[2].y = 240;    
+    cobra->segmentos[3].x = 290;
+    cobra->segmentos[3].y = 240;    
     cobra->direcao = DIREITA;
-    cobra->tamanho = 4;
+    cobra->tamanho = 1;
     return cobra;
+}
+
+
+void apelido(void){
+    char *apelido[30];
+    printf("Como voce gostaria de ser chamado? \n");
+    scanf("%s", *apelido);
+
+}
+
+
+void recordes(void){
+	system("cls");
+	char texto_str[20];
+	FILE *arquivo = fopen("recordes.txt", "r");
+
+	if(arquivo!=NULL){
+		printf("Listando:\n");
+		while(fgets(texto_str, 20, arquivo) != NULL){
+			printf("%s", texto_str);
+		}
+
+		fclose(arquivo);
+	}else{
+		printf("Nenhum recorde registrado!\n");
+	}
+
+	
+	system("cls");
+
 }
 
 void surgir_fruta (Fruta *fruta){
     if(fruta->ativo==0)
     {
         srand(time(NULL));
-        fruta->segmento.x = rand() % 630;
-        fruta->segmento.y = rand() % 470;
+        fruta->segmento.x = (rand() % 63)*10;
+        fruta->segmento.y = (rand() % 47)*10;
         fruta->ativo = 1;
     }
 
@@ -55,6 +87,11 @@ void surgir_fruta (Fruta *fruta){
 
 void limite_de_tela(Cobra *cobra){
     if (cobra->segmentos[0].x<0 || cobra->segmentos[0].x>640 || cobra->segmentos[0].y<0 || cobra->segmentos[0].y>480){
+        fonte("assets/ubuntu.ttf", 20);
+        desenha_texto(320, 240, "GAME OVER.");
+        toca_som("assets/jump.wav");
+                                                    // nao ta aparecendo game over
+        sleep(1);
         janela_deve_fechar();
     }
     
@@ -101,14 +138,15 @@ void desenha_fruta(Fruta *fruta){
 
 void colisao_fruta(Fruta *fruta, Cobra *cobra){
    
-    if(cobra->segmentos[0].x<fruta->segmento.x && fruta->segmento.x<cobra->segmentos[0].x+10 && 
-    cobra->segmentos[0].y<fruta->segmento.y && fruta->segmento.y<cobra->segmentos[0].y+10) {
+    if( (cobra->segmentos[0].x < fruta->segmento.x+1 && fruta->segmento.x+1 < cobra->segmentos[0].x+10) && 
+        (cobra->segmentos[0].y < fruta->segmento.y+1 && fruta->segmento.y+1 < cobra->segmentos[0].y+10)) {
         fruta->ativo=0;
         cobra->segmentos[cobra->tamanho].x = cobra->segmentos[cobra->tamanho-1].x;
         cobra->segmentos[cobra->tamanho].y = cobra->segmentos[cobra->tamanho-1].y;
 
         cobra->tamanho+=1;
-        
+
+        toca_som("assets/comer.wav");
         
     }
     
@@ -117,19 +155,9 @@ void colisao_fruta(Fruta *fruta, Cobra *cobra){
 }
 
 
-
-int main() {
-
-    // char *apelido = malloc(sizeof (char)*1000);
-
-    // printf("Digite seu apelido: ");
-    // fgets(apelido, 30, stdin);
-
-    // FILE *arq_apelidos = fopen("arq_apelidos.txt", "W");
-    // fwrite(apelido, 1, sizeof (apelido), arq_apelidos);
-
-
+void lenta (){
     abre_janela(640, 480);
+    toca_som_em_loop("assets/awesomeness.wav");
     Cobra *cobra = cria_cobra();
     Fruta *fruta = (Fruta*)malloc(sizeof(Fruta));
     fruta->ativo = 0;
@@ -139,8 +167,10 @@ int main() {
          if (tecla_pressionada(ESC))
             janela_deve_fechar();
 
+       
+
         fonte("assets/ubuntu.ttf", 20);
-        desenha_texto(20, 20, "Score: %d", cobra->tamanho-4);
+        desenha_texto(20, 20, "Score: %d", cobra->tamanho-1);
 
         atualiza_cobra(cobra);
         limite_de_tela(cobra);
@@ -148,8 +178,6 @@ int main() {
         colisao_fruta(fruta, cobra);
         surgir_fruta(fruta);
         desenha_fruta(fruta);
-        
-
         
 
 
@@ -162,9 +190,150 @@ int main() {
         if (tecla_pressionada(DIREITA) && cobra->direcao != ESQUERDA) 
             cobra->direcao = DIREITA;
         int i=0;
-        while(i < 50000000){ //50000000 10000000
+        while(i < 75000000){ //50000000 10000000
             i++;
         }
     }
     fecha_janela();
+
+}
+
+void normal (){
+    abre_janela(640, 480);
+    toca_som_em_loop("assets/awesomeness.wav");
+    Cobra *cobra = cria_cobra();
+    Fruta *fruta = (Fruta*)malloc(sizeof(Fruta));
+    fruta->ativo = 0;
+
+    while (janela_esta_aberta()) {
+
+         if (tecla_pressionada(ESC))
+            janela_deve_fechar();
+
+       
+
+        fonte("assets/ubuntu.ttf", 20);
+        desenha_texto(20, 20, "Score: %d", cobra->tamanho-1);
+
+        atualiza_cobra(cobra);
+        limite_de_tela(cobra);
+        desenha_cobra(cobra);
+        colisao_fruta(fruta, cobra);
+        surgir_fruta(fruta);
+        desenha_fruta(fruta);
+        
+
+
+        if (tecla_pressionada(CIMA) && cobra->direcao != BAIXO) 
+            cobra->direcao = CIMA;
+        if (tecla_pressionada(BAIXO) && cobra->direcao != CIMA) 
+            cobra->direcao = BAIXO;
+        if (tecla_pressionada(ESQUERDA) && cobra->direcao != DIREITA) 
+            cobra->direcao = ESQUERDA;
+        if (tecla_pressionada(DIREITA) && cobra->direcao != ESQUERDA) 
+            cobra->direcao = DIREITA;
+        int i=0;
+        while(i < 55000000){ //50000000 10000000
+            i++;
+        }
+    }
+    fecha_janela();
+}
+
+void rapida (){
+    abre_janela(640, 480);
+    toca_som_em_loop("assets/awesomeness.wav");
+    Cobra *cobra = cria_cobra();
+    Fruta *fruta = (Fruta*)malloc(sizeof(Fruta));
+    fruta->ativo = 0;
+
+    while (janela_esta_aberta()) {
+
+         if (tecla_pressionada(ESC))
+            janela_deve_fechar();
+
+       
+
+        fonte("assets/ubuntu.ttf", 20);
+        desenha_texto(20, 20, "Score: %d", cobra->tamanho-1);
+
+        atualiza_cobra(cobra);
+        limite_de_tela(cobra);
+        desenha_cobra(cobra);
+        colisao_fruta(fruta, cobra);
+        surgir_fruta(fruta);
+        desenha_fruta(fruta);
+        
+
+
+        if (tecla_pressionada(CIMA) && cobra->direcao != BAIXO) 
+            cobra->direcao = CIMA;
+        if (tecla_pressionada(BAIXO) && cobra->direcao != CIMA) 
+            cobra->direcao = BAIXO;
+        if (tecla_pressionada(ESQUERDA) && cobra->direcao != DIREITA) 
+            cobra->direcao = ESQUERDA;
+        if (tecla_pressionada(DIREITA) && cobra->direcao != ESQUERDA) 
+            cobra->direcao = DIREITA;
+        int i=0;
+        while(i < 30000000){ //50000000 10000000
+            i++;
+        }
+    }
+    fecha_janela();
+}
+
+
+
+int main() {
+
+    //passar pra função 
+
+    int menu;
+    system("clear");
+    printf("Escolha uma opção para começar: \n");
+    printf("1 - Inicar jogo.\n");
+    printf("2 - Ver recordes.\n");
+    printf("3 - Sair do jogo.\n");
+    scanf("%d", &menu);
+
+    switch (menu) {
+        
+    case 1:
+        apelido();
+        int velocidade;
+        printf("Qual a velocidade do jogo? \n");
+        printf("1 - Lento. \n2 - Normal\n3 - Rapido\n");
+        scanf("%d", &velocidade);
+
+        switch (velocidade)
+{
+        case 1:
+            lenta();
+            break;
+        case 2:
+            normal();
+            break;
+        case 3:
+            rapida();
+
+        default:
+            printf("Opcao invalida!.");
+            break;
+        }
+
+
+
+        break;
+    case 2:
+        recordes();
+        break;
+    case 3:
+        exit(0);
+    default:
+        printf("Digite uma opcao valida!");
+        break;
+    }
+
+
+    
 }
